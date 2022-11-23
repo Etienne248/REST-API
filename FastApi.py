f657d2e24389675b7cefc4aa7dd3529c8af6f9c9
Etienne Pommel <etienne.pommel@gmail.com>
2022-11-23 18:05:11 +0100

    khj

--- FastApi.py
+++ FastApi.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+import datetime
+import json
 
 app = FastAPI(name="Test")
 
-donnees = {"messages":"hello word"}
 @app.get("/")
-def messages():
-    return donnees
+async def root():
+    return {"message": "Hello World"}
+
+class Todo:
+    def __init__(self, ID : int,Title : str, State : int, Due_date : str , Description : str ):
+        self.ID=ID
+        self.Title=Title
+        self.State= State  #(0 : todo, 1 : done)
+        self.Due_date = Due_date
+        self.Description= Description
+
+todoList = []
+
+@app.get("/todo")
+async def get_Todo():
+    return {"todoList" : json.dumps(todoList)}  
+
+@app.post("/todo")
+async def create_Todo(todo = Todo):
+    newtodo = Todo()
+    todoList.append(newtodo)
+    return {"message": "post created"}
